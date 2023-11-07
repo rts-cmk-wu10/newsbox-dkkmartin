0d38f828d1db38de297f785f422ff1d0566fdad3
Martin <martinbruun1995@gmail.com>
2023-11-07 22:52:47 +0100

    Added archive page

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,6 @@
 import '../scss/styles.scss';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/offcanvas';
+import 'bootstrap/js/dist/collapse';
