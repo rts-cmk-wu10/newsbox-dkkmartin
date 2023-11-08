134af869e25f026a57580315ac557b86ef30b2bc
Martin <martinbruun1995@gmail.com>
2023-11-09 00:02:45 +0100

    Storage and settings js

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,29 +1,33 @@
 //@ts-check
-
-export function settingsSaver() {
-  const settingsArray = [];
-  const settingsInputs = document.querySelectorAll('.settings__container > div > div > input');
-  settingsInputs.forEach((input) => {
-    input.addEventListener('click', (e) => {
-      console.log(settingsArray);
-      let found = false;
-      settingsArray.forEach((obj) => {
-        if (obj.name === input.id) {
-          obj.checked = input.checked;
-          found = true;
+import Storage from './storage';
+export default class Settings {
+  static settingsSaver() {
+    const settingsInputs = document.querySelectorAll('.settings__container > div > div > input');
+    const settingsArray = [];
+    settingsInputs.forEach((input) => {
+      input.addEventListener('click', () => {
+        console.log(settingsArray);
+        let found = false;
+        settingsArray.forEach((obj) => {
+          if (obj.name === input.id) {
+            obj.checked = input.checked;
+            found = true;
+          }
+        });
+        if (!found) {
+          const newSetting = {
+            name: input.id,
+            checked: input.checked,
+          };
+          settingsArray.push(newSetting);
         }
+        Storage.saveToStorage('settings', settingsArray);
+        console.log('LocalStorage: ', Storage.loadFromStorage('settings'));
       });
-      if (!found) {
-        const newSetting = {
-          name: input.id,
-          checked: input.checked,
-        };
-        settingsArray.push(newSetting);
-      }
     });
-  });
+  }
+
+  static settingsLoader() {}
 }
 
-export function settingsLoader() {}
-
-settingsSaver();
+Settings.settingsSaver();
--- src/js/storage.js
+++ src/js/storage.js
@@ -1,8 +1,17 @@
-export function saveToStorage(name, content) {
-  const stringContent = JSON.stringify(content);
-  localStorage.setItem(name, stringContent);
+export default class Storage {
+  static saveToStorage(name, content) {
+    if (localStorage.getItem(name)) {
+      this.modifyStorage(name, content);
+    } else {
+      const stringContent = JSON.stringify(content);
+      localStorage.setItem(name, stringContent);
+    }
+  }
+
+  static modifyStorage(name, content) {}
+
+  static loadFromStorage(name) {
+    const content = JSON.parse(localStorage.getItem(name));
+    return content;
+  }
 }
-
-export function modifyStorage() {}
-
-export function loadFromStorage() {}
