5016256cb922ad25795186a33614b2f8d5a26cdb
Martin <martinbruun1995@gmail.com>
2023-11-08 01:34:35 +0100

    Settings js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 // Import our custom CSS
 import '../scss/styles.scss';
 
-// Import all of Bootstrap's JS
+// Import needed of Bootstrap's JS
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/offcanvas';
 import 'bootstrap/js/dist/collapse';
+
+// Import local scripts
+import './settings';
---
+++ src/js/settings.js
@@ -0,0 +1,8 @@
+//@ts-check
+
+export default function settingsHandler() {
+  const settingsDiv = document.querySelector('.settings__container');
+  console.log(settingsDiv);
+}
+
+settingsHandler();
